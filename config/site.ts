035d322a1c5f8d400b74f798b20d467c9aa65656
socioholmes <sociokana@gmail.com>
2023-07-11 08:22:04 +0900

    added pages

--- config/site.ts
+++ config/site.ts
@@ -3,9 +3,17 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   mainNav: [
     {
-      title: "",
-      href: "",
-    }
+      title: "Community handle",
+      href: "/",
+    },
+    {
+      title: "핸들 변경하기",
+      href: "/korean",
+    },
+    {
+      title: "ハンドル取得",
+      href: "/japanese",
+    },
   ],
   links: {
     github: "https://github.com/mozzius/community-handles",
